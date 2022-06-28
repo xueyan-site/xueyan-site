639c771107f88a7f6cd09a8f4e90529a9404397b
xueyan-site <xueyan@xueyan.site>
2022-06-28 10:56:09 +0800

    chore: update resume

--- src/resume/index.tsx
+++ src/resume/index.tsx
@@ -19,13 +19,19 @@ export default function Index() {
           <div>
             曾就职于 <strong>拼多多</strong>、<strong>小红书</strong> ，从事电商领域前端研发工作。
           </div>
-          <div>
-            寻求 <strong>成都地区</strong>，以 <strong>React</strong> 或 <strong>Vue</strong> 为主要技术栈的前端岗位。
-          </div>
         </div>
         <div className={styles.section}>
           <div>
-            特点：了解电商；有前端审美能力；有 SSR 项目开发经验；熟悉装修模式（后台编辑页面至前台渲染）；
+            从事电商五年，了解电商相关业务，做过批发市场，参与过多多买菜初期建设，能触类旁通；
+          </div>
+          <div>
+            负责装修工作五年，熟悉 “后台编辑页面至前台动态渲染” 相关业务模式；
+          </div>
+          <div>
+            曾管理批发和店铺业务线，主导建立过批发和店铺许多重要业务，负责过相关项目 SSR 迁移工作；
+          </div>
+          <div>
+            有前端审美能力，为商家端开发过许多组件和工具库，对代码独立、易扩展的重要性有深刻认识；
           </div>
         </div>
         <div className={styles.section}>
@@ -34,10 +40,10 @@ export default function Index() {
         <div className={styles.section}>
           <div className={styles.block}>
             <strong>2019年初至2020年上半年</strong>，独立负责商家装修前端工作，开发了店铺首页、店铺专题页、装修模版市场等装修业务。
-            期间研发的装修框架，被服务于拼多多后台所有装修和类装修模式的业务。
+            期间研发的装修框架，被服务于商家端所有装修和类装修模式的业务。
           </div>
           <div className={styles.block}>
-            <strong>2020年下半年</strong>，先加入商家小程序的建设，后参与研发并管理批发平台（成员人数在2～4人间）。
+            <strong>2020年下半年</strong>，参与商家小程序的建设，后参与研发并管理批发平台（成员人数在2～4人间）。
           </div>
           <div className={styles.block}>
             <strong>2021年初至今</strong>，管理商家店铺前端业务线（包括装修、店铺、平台治理三个板块，成员人数在3～6人间），并负责部分项目的 SSR 改造，附带管理批发平台（后期交给其他组负责）。
@@ -55,20 +61,21 @@ export default function Index() {
             },
             {
               name: 'xueyan-react-transition',
-              desc: '类似 Vue Transition 的 React 过渡组件'
+              desc: 'React 过渡动画组件'
             },
             {
               name: 'xueyan-typescript-cli',
-              desc: '开发 React 项目/组件、Node/Web 模块的 TS 脚手架'
+              desc: 'TS 脚手架，支持低成本开发 React 项目/组件、Node/Web 模块'
             }
           ].map(item => (
             <div key={item.name}>
               <a className={styles.plabel} href={`https://xueyan.site/${item.name}`} target="_blank">
                 {item.name}
               </a>
-              (<a href={`https://github.com/xueyan-site/${item.name}`} target="_blank">
+              （<a href={`https://github.com/xueyan-site/${item.name}`} target="_blank">
                 源码
-              </a>) {item.desc}
+              </a>）
+              {item.desc}
             </div>
           ))}
         </div>
